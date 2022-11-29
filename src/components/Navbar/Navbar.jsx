06ec78fba7a30c40896bc890b1ea682c7ac2c836
김진영C <jin0kim326@douzone.com>
2022-11-29 16:41:53 +0900

    navbar 기능 구현 (검색어 미적용...ㅠㅠ)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import React, { useState } from 'react';
 
 export default function Navbar() {
+    const [query, setQuery] = useState(''); //검색어
+    const client = useQueryClient();
+
+    const {
+        isLoading,
+        error,
+        data: searchVideoList,
+      } = useQuery(['searchVideoList'], async () => {
+        return fetchVideosByQuery(query);
+      },{
+        staleTime: 6000
+      });
+    
+
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getSearchList();
+    }
+
+    const getSearchList= () =>{
+        client.invalidateQueries(['searchVideoList']);
+    }
+
+    const fetchVideosByQuery = async (query) => {
+        console.log(`:: 검색결과 조회완료 검색어:${query} ::`);
+        return fetch(`data/searchVideoList.json`).then((res) => res.json());
+    }
+
+    
+
+
+    const handleChange = (e) => {
+        const {value: query} = e.target;
+        setQuery(query);
+    }
+
     return (
-        <div>
-            <h1>navbar</h1>
-        </div>
+        <>
+            <form action="" onSubmit={handleSubmit} >
+                <input type="text" name='q' onChange={handleChange}/>
+                <button>search</button>
+            </form>
+        </>
     );
 }
 
