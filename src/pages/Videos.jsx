b241b1c71a5c6510fa4e789801da7e390c1e242e
김진영C <jin0kim326@douzone.com>
2022-11-30 15:19:13 +0900

    12.12 fake서비스 / 진짜 서비스

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard/VideoCard';
 import axios from 'axios';
+import Youtube from '../api/youtube';
+import FakeYoutube from '../api/fakeYoutube';
 
 
 export default function Videos() {
@@ -13,8 +15,8 @@ export default function Videos() {
         error,
         data: videos,
     } = useQuery(['videos',keyword], async () => {
-        return axios.get(`/videos/${keyword ? 'search':'popular'}.json`)
-        .then((res) => res.data.items)
+        const youtube = new Youtube();
+        return youtube.search(keyword);
     });
 
     return (
