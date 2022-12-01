f30823a0a8d3089b1e963a8c444bfa8f499afddd
김진영C <jin0kim326@douzone.com>
2022-12-01 17:39:31 +0900

    12.15 비디오카드 기능 (timeago.js사용)

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { formatAgo } from '../../util/date';
 
 export default function VideoCard({video}) {
+    const {title, thumbnails, channelTitle,publishedAt} = video.snippet;
     return (
-        <div>
-            {video.snippet.title}
-        </div>
+        <li>
+            <img src={thumbnails.medium.url} alt={title}/>
+            <div>
+                <p>{title}</p>
+                <p>{channelTitle}</p>
+                <p>{formatAgo(publishedAt)}</p>
+            </div>
+        </li>
     );
 }
 
