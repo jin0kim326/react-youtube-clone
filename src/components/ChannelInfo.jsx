daa74996381ba923e79eb9435a3b061bc75d8348
김진영C <jin0kim326@douzone.com>
2022-12-02 13:20:17 +0900

    12.20 비디오 상세 스타일링

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -12,9 +12,9 @@ export default function ChannelInfo({ id, name }) {
   } = useQuery(['channel', id], () => youtube.channelImageURL(id));
 
   return (
-    <div>
-      {url && <img src={url} alt={name} />}
-      <p>{name}</p>
+    <div className='flex my-4 mb-8 items-center'>
+      {url && <img className='w-10 h-10 rounded-full' src={url} alt={name} />}
+      <p className='text-lg font-medium'>{name}</p>
     </div>
   );
 }
